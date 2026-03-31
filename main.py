9f08a32487778b988381e9896036c373a0de4745
60232294gsb <getspicybear@mju.ac.kr>
2026-03-31 15:12:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-# 파일이름 :
-# 작 성 자 :
+# 파일이름 : 4주차 미션
+# 작 성 자 : 60232294 신지웅
+
+#미션 3
+bucket_list = []
+
+bucket_list.append(input("맛집 리스트 입력: "))
+bucket_list.append(input("맛집 리스트 입력: "))
+bucket_list.append(input("맛집 리스트 입력: "))
+
+print("리스트:", bucket_list)
+
+bucket_list.insert(0, input("맛집 리스트 추가: "))
+print("리스트:", bucket_list)
+
+bucket_list.remove(input("도장 깨기: "))
+print("리스트:", bucket_list)
